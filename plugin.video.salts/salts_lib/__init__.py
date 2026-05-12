211159db077e450cfb8f63e98de762cf670e5773
Zeus768 <gowerconor2013@gmail.com>
2026-05-13 00:26:53 +0100

    Add files via upload

---
+++ plugin.video.salts/salts_lib/__init__.py
@@ -0,0 +1,10 @@
+"""
+SALTS Library
+Revived by zeus768 for Kodi 21+
+"""
+from . import constants
+from . import log_utils
+from . import db_utils
+from . import utils
+from . import debrid
+from . import trakt_api
